af9ce45c1505ba9b99b7144fba01ce9d249a9f55
Michael.Erickson <merickson51293@gmail.com>
2020-12-16 13:34:54 -0500

    added goods models

--- sweet_petite_app/models.py
+++ sweet_petite_app/models.py
@@ -16,3 +16,9 @@ class Review(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     objects=ReviewManager()
+
+class Goods(models.Model):
+    name=models.CharField(max_length=255)
+    price=models.FloatField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
