eb482614c680a0421b2219fa44deca6546d29f7d
Michael.Erickson <merickson51293@gmail.com>
2021-01-12 10:33:56 -0500

    added order page

--- sweet_petite_app/views.py
+++ sweet_petite_app/views.py
@@ -79,30 +79,6 @@ def blog(request):
     }
     return render(request, "blog.html", context)
 
-def checkout(request):
-    last = Order.objects.last()
-    price=last.total_price
-    full_order = Order.objects.aggregate(Sum('quantity_ordered'))['quantity_ordered__sum']
-    full_price = Order.objects.aggregate(Sum('total_price'))['total_price__sum']
-    context = {
-        'orders':full_order,
-        'total':full_price,
-        'bill':price,
-    }
-    return render(request, "store/checkout.html",context)
-
-def purchase(request):
-    if request.method == 'POST':
-        this_product = Goods.objects.filter(id=request.POST["id"])
-        if not this_product:
-            return redirect('/')
-        else:
-            quantity = int(request.POST["quantity"])
-            total_charge = quantity*(float(this_product[0].price))
-            Order.objects.create(quantity_ordered=quantity, total_price=total_charge)
-            return redirect('/checkout')
-    else:
-        return redirect('/')
 
 def good(request, goods_id):
     context={
